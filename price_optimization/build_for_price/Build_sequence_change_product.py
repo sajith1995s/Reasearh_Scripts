4b80d95f26262ef684cc1c2a35632b7075dc0b4b
sachithkk <tharakakk@gmail.com>
2019-11-09 10:01:14 +0530

    commeted by sachith

--- price_optimization/build_for_price/Build_sequence_change_product.py
+++ price_optimization/build_for_price/Build_sequence_change_product.py
@@ -1,55 +1,192 @@
+# from pymongo import MongoClient
+# import sys
+#
+# want_product = sys.argv[1]
+# price_range = sys.argv[2]
+#
+# motherboard = ''
+# cpu = ''
+# ram = ''
+# vga = ''
+#
+# res = []
+#
+# client = MongoClient('mongodb://localhost:27017')
+# db = client.techRingdb
+#
+# if motherboard != "":
+#     col_motherboard = db.Motherboard
+#     motherboard_pro = col_motherboard.find({"id": motherboard})
+#
+# if cpu != "":
+#     col_cpu = db.CPU
+#     cpu_pro = col_cpu.find({"id": cpu})
+#
+# if ram != "":
+#     col_ram = db.RAM
+#     ram_pro = col_ram.find({"id": ram})
+#
+# if vga != "":
+#     col_vga = db.VGA
+#     vga_pro = col_vga.find({"id": col_vga})
+#
+# if want_product == "motherboard":
+#     for col in col_motherboard.find():
+#         if (col["cpu_brand"] in cpu_pro["socket"]) or (cpu_pro["socket"] in col["cpu_brand"]):
+#             if col["memory_type"] == ram_pro["type"]:
+#                 if col["pci_slot"] == vga_pro["slot"]:
+#                     res.append(col)
+#
+# elif want_product == "cpu":
+#     for col in col_cpu.find():
+#         if (col["socket"] in motherboard_pro["cpu_brand"]) or (motherboard_pro["cpu_brand"] in col["socket"]):
+#             res.append(col)
+#
+# elif want_product == "ram":
+#     for col in col_ram.find():
+#         if (col["type"] in motherboard_pro["memory_type"]) or (motherboard_pro["memory_type"] in col["type"]):
+#             res.append(col)
+#
+# elif want_product == "vga":
+#     for col in col_vga.find():
+#         if (col["pci_slot"] in vga_pro["slot"]) or (vga_pro["slot"] in col["pci_slot"]):
+#             res.append(col)
+#
+# print(res)
+
+
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 import sys
-
-want_product = sys.argv[1]
-price_range = sys.argv[2]
-
-motherboard = ''
-cpu = ''
-ram = ''
-vga = ''
-
-res = []
 
 client = MongoClient('mongodb://localhost:27017')
 db = client.techRingdb
 
-if motherboard != "":
-    col_motherboard = db.Motherboard
-    motherboard_pro = col_motherboard.find({"id": motherboard})
+want_product = sys.argv[1]
+min_price = sys.argv[2]
+max_price = sys.argv[3]
 
-if cpu != "":
-    col_cpu = db.CPU
-    cpu_pro = col_cpu.find({"id": cpu})
+# want_product = "cpu"
+# min_price = "0"
+# max_price = "25000"
 
-if ram != "":
-    col_ram = db.RAM
-    ram_pro = col_ram.find({"id": ram})
+# cpu_arr = "5d6aabfac9c3e2a971b5cd77"
+cpu_arr = sys.argv[4]
+cpu_new_arr = []
+# motherboard_arr = "5d6ab2a6204a1c169b0422f9"
+motherboard_arr = sys.argv[5]
+motherboard_new_arr = []
+# ram_arr = "5d6ab5bec863baf5005fa70c"
+ram_arr = sys.argv[6]
+ram_new_arr = []
+# vga_arr = "5d6ac0e0c31e8d690f3033bd"
+vga_arr = sys.argv[7]
+vga_new_arr = []
+# hard_disk_arr = "5d6aaf377e6f92a252a6e731"
+hard_disk_arr = sys.argv[8]
+hard_disk_new_arr = []
 
-if vga != "":
-    col_vga = db.VGA
-    vga_pro = col_vga.find({"id": col_vga})
+result = ''
+myquery1 = { "_id": ObjectId(cpu_arr)}
+myquery2 = { "_id": ObjectId(motherboard_arr)}
+myquery3 = { "_id": ObjectId(ram_arr)}
+myquery4 = { "_id": ObjectId(vga_arr)}
+myquery5 = { "_id": ObjectId(hard_disk_arr)}
+
+cpu = db.CPU
+cpu_new_arr = cpu.find_one(myquery1)
+motherboard = db.Motherboard
+motherboard_new_arr = motherboard.find_one(myquery2)
+ram = db.RAM
+ram_new_arr = ram.find_one(myquery3)
+vga = db.VGA
+vga_new_arr = vga.find_one(myquery4)
+hard_disk = db.Hard_Disk
+hard_disk_new_arr = hard_disk.find_one(myquery5)
+
+
+def check_compatibility(pro_name, pro):
+    if pro_name == "motherboard":
+        if (cpu_new_arr["socket"] == pro["socket"]) or (cpu_new_arr["socket"] in pro["socket"]) or (pro["socket"] in cpu_new_arr["socket"]):
+            return True
+        return False
+    if pro_name == "ram":
+        if (motherboard_new_arr["type"] == pro["type"]) or (motherboard_new_arr["type"] in pro["type"]) or (pro["type"] in motherboard_new_arr["type"]):
+            return True
+        return False
+    if pro_name == "vga":
+        # if (motherboard_arr["slot"] == pro["pci_slot"]) or (motherboard_arr["slot"] in pro["pci_slot"]) or (pro["pci_slot"] == motherboard_arr["slot"]):
+        #     return True
+        return True
+
+def checkPrice(word):
+    for x in word:
+        if not x.isdigit():
+            if x != ".":
+                return False
+    return True
+
+if want_product == "cpu":
+    for record in cpu.find().sort("ratings"):
+        if record["price"] != "" and record["price"] != " ":
+            x = checkPrice(record["price"])
+            if x == True:
+                if float(min_price) <= float(record['price']) and float(max_price) >= float(record["price"]):
+                    if cpu_arr != record["_id"]:
+                        y = check_compatibility(want_product, cpu_new_arr)
+                        if y == True:
+                            result = record
+                            break
 
 if want_product == "motherboard":
-    for col in col_motherboard.find():
-        if (col["cpu_brand"] in cpu_pro["socket"]) or (cpu_pro["socket"] in col["cpu_brand"]):
-            if col["memory_type"] == ram_pro["type"]:
-                if col["pci_slot"] == vga_pro["slot"]:
-                    res.append(col)
 
-elif want_product == "cpu":
-    for col in col_cpu.find():
-        if (col["socket"] in motherboard_pro["cpu_brand"]) or (motherboard_pro["cpu_brand"] in col["socket"]):
-            res.append(col)
+    for record in motherboard.find().sort("ratings"):
+        if record["price"] != "" and record["price"] != " ":
+            x = checkPrice(record["price"])
+            y = checkPrice(min_price)
+            if x == True and y == True:
+                if float(min_price) <= float(record['price']) and float(max_price) >= float(record["price"]):
+                    if motherboard_arr != record["_id"]:
+                        y = check_compatibility(want_product, motherboard_new_arr)
+                        if y == True:
+                            result = record
+                            break
 
-elif want_product == "ram":
-    for col in col_ram.find():
-        if (col["type"] in motherboard_pro["memory_type"]) or (motherboard_pro["memory_type"] in col["type"]):
-            res.append(col)
+if want_product == "ram":
 
-elif want_product == "vga":
-    for col in col_vga.find():
-        if (col["pci_slot"] in vga_pro["slot"]) or (vga_pro["slot"] in col["pci_slot"]):
-            res.append(col)
+    for record in ram.find().sort("ratings"):
+        if record["price"] != "" and record["price"] != " ":
+            x = checkPrice(record["price"])
+            if x == True:
+                if float(min_price) <= float(record['price']) and float(max_price) >= float(record["price"]):
+                    if ram_arr != record["_id"]:
+                        y = check_compatibility(want_product, ram_new_arr)
+                        if y == True:
+                            result = record
+                            break
 
-print(res)
+if want_product == "vga":
+
+    for record in vga.find().sort("ratings"):
+        if record["price"] != "" and record["price"] != " ":
+            x = checkPrice(record["price"])
+            if x == True:
+                if float(min_price) <= float(record['price']) and float(max_price) >= float(record["price"]):
+                    if vga_arr != record["_id"]:
+                        y = check_compatibility(want_product, vga_new_arr)
+                        if y == True:
+                            result = record
+                            break
+
+if want_product == "hard_disk":
+
+    for record in hard_disk.find().sort("ratings"):
+        if record["price"] != "" and record["price"] != " ":
+            x = checkPrice(record["price"])
+            if x == True:
+                if float(min_price) <= float(record['price']) and float(max_price) >= float(record["price"]):
+                    if hard_disk_arr != record["_id"]:
+                        result = record
+                        break
+
+print(result)
--- price_optimization/build_for_price/Build_sequence_price_range.py
+++ price_optimization/build_for_price/Build_sequence_price_range.py
@@ -1,3 +1,116 @@
+# from pymongo import MongoClient
+# import sys
+#
+# ram_min = sys.argv[1]
+# ram_max = sys.argv[2]
+# motherboard_min = sys.argv[3]
+# motherboard_max = sys.argv[4]
+# vga_min = sys.argv[5]
+# vga_max = sys.argv[6]
+# cpu_min = sys.argv[7]
+# cpu_max = sys.argv[8]
+# hard_disk_min = sys.argv[9]
+# hard_disk_max = sys.argv[10]
+#
+# ram_arr = []
+# vga_arr = []
+# cpu_arr = []
+# motherboard_arr = []
+# hard_disk_arr = []
+#
+# build_arr = []
+#
+# client = MongoClient('mongodb://localhost:27017')
+# db = client.techRingdb
+#
+# def check_compatibility(pro_name, pro, arr):
+#     if pro_name == "motherboard":
+#         for item in arr:
+#             if item == "cpu":
+#                 if arr[1]["socket"] in pro["cpu_brand"]:
+#                     return True
+#                 elif pro["cpu_brand"] in arr[1]["socket"]:
+#                     return True
+#         return False
+#     elif pro_name == "ram":
+#         for item in arr:
+#             if item == "motherboard":
+#                 if arr[3]["memory_type"] in pro["type"]:
+#                     return True
+#                 elif pro["type"] in arr[3]["memory_type"]:
+#                     return True
+#         return False
+#     elif pro_name == "vga":
+#         for item in arr:
+#             if item == "motherboard":
+#                 if arr[3]["pci_slot"] in pro["slot"]:
+#                     return True
+#                 elif pro["slot"] in arr[3]["pci_slot"]:
+#                     return True
+#         return False
+#
+# def build_sequence(lists, category):
+#     if category == "cpu":
+#         build_arr.append("cpu")
+#         for item in lists:
+#             build_arr.append(item)
+#             break
+#     elif category == "motherboard":
+#         build_arr.append("motherboard")
+#         for item in lists:
+#             x = check_compatibility("motherboard", item, build_arr)
+#             if x==True:
+#                 build_arr.append(item)
+#                 break
+#     elif category == "ram":
+#         build_arr.append("ram")
+#         for item in lists:
+#             x = check_compatibility("ram", item, build_arr)
+#             if x==True:
+#                 build_arr.append(item)
+#                 break
+#     elif category == "vga":
+#         build_arr.append("vga")
+#         for item in lists:
+#             x = check_compatibility("vga", item, build_arr)
+#             if x == True:
+#                 build_arr.append(item)
+#                 break
+#
+# cpu = db.CPU
+# for record in cpu.find().sort("points"):
+#     if cpu_min <= float(record['price']) and cpu_max >= float(record["price"]):
+#         cpu_arr.append(record)
+# build_sequence(cpu_arr, "cpu")
+#
+# motherboard = db.Motherboard
+# for record in motherboard.find().sort("points"):
+#     if motherboard_min <= float(record['price']) and motherboard_max >= float(record["price"]):
+#         motherboard_arr.append(record)
+# build_sequence(motherboard_arr, "motherboard")
+#
+# ram = db.RAM
+# for record in ram.find().sort("points"):
+#     if ram_min <= float(record['price']) and ram_max >= float(record["price"]):
+#         ram_arr.append(record)
+# build_sequence(ram_arr, "ram")
+#
+# vga = db.VGA
+# for record in vga.find().sort("points"):
+#     if vga_min <= float(record['price']) and vga_max >= float(record["price"]):
+#         vga_arr.append(record)
+# build_sequence(vga_arr, "vga")
+#
+# hard_disk = db.Hard_Disk
+# for record in hard_disk.find().sort("points").limit(1):
+#     if hard_disk_min <= float(record['price']) and hard_disk_max >= float(record['price']):
+#         hard_disk_arr.append(record)
+#         build_arr.append("hard_disk")
+#         build_arr.append(record)
+#
+# # for x in build_arr:
+# #     print(x)
+
 from pymongo import MongoClient
 import sys
 
@@ -12,6 +125,17 @@ cpu_max = sys.argv[8]
 hard_disk_min = sys.argv[9]
 hard_disk_max = sys.argv[10]
 
+# ram_min = "0"
+# ram_max = "10000"
+# motherboard_min = "0"
+# motherboard_max = "10000"
+# vga_min = "0"
+# vga_max = "10000"
+# cpu_min = "0"
+# cpu_max = "10000"
+# hard_disk_min = "0"
+# hard_disk_max = "10000"
+
 ram_arr = []
 vga_arr = []
 cpu_arr = []
@@ -23,90 +147,102 @@ build_arr = []
 client = MongoClient('mongodb://localhost:27017')
 db = client.techRingdb
 
-def check_compatibility(pro_name, pro, arr):
+def check_compatibility(pro_name, pro, prev):
     if pro_name == "motherboard":
-        for item in arr:
-            if item == "cpu":
-                if arr[1]["socket"] in pro["cpu_brand"]:
-                    return True
-                elif pro["cpu_brand"] in arr[1]["socket"]:
-                    return True
+        if (prev["socket"] == pro["socket"]) or (prev["socket"] in pro["socket"]) or (pro["socket"] in prev["socket"]):
+            return True
         return False
-    elif pro_name == "ram":
-        for item in arr:
-            if item == "motherboard":
-                if arr[3]["memory_type"] in pro["type"]:
-                    return True
-                elif pro["type"] in arr[3]["memory_type"]:
-                    return True
+    if pro_name == "ram":
+        if (prev["type"] == pro["type"]) or (prev["type"] in pro["type"]) or (pro["type"] == prev["type"]):
+            return True
         return False
-    elif pro_name == "vga":
-        for item in arr:
-            if item == "motherboard":
-                if arr[3]["pci_slot"] in pro["slot"]:
-                    return True
-                elif pro["slot"] in arr[3]["pci_slot"]:
-                    return True
-        return False
+    if pro_name == "vga":
+        # if (prev["slot"] == pro["pci_slot"]) or (prev["slot"] in pro["pci_slot"]) or (pro["pci_slot"] == prev["slot"]):
+        #     return True
+        return True
 
-def build_sequence(lists, category):
-    if category == "cpu":
-        build_arr.append("cpu")
-        for item in lists:
-            build_arr.append(item)
-            break
-    elif category == "motherboard":
-        build_arr.append("motherboard")
-        for item in lists:
-            x = check_compatibility("motherboard", item, build_arr)
-            if x==True:
-                build_arr.append(item)
-                break
-    elif category == "ram":
-        build_arr.append("ram")
-        for item in lists:
-            x = check_compatibility("ram", item, build_arr)
-            if x==True:
-                build_arr.append(item)
-                break
-    elif category == "vga":
-        build_arr.append("vga")
-        for item in lists:
-            x = check_compatibility("vga", item, build_arr)
-            if x == True:
-                build_arr.append(item)
-                break
+
+
+def build_sequence():
+    for a in cpu_arr:
+        for b in motherboard_arr:
+            res1 = check_compatibility("motherboard", b, a)
+            if res1 == True:
+                val1 = False
+                for c in ram_arr:
+                    res1 = check_compatibility("ram", c, b)
+                    if res1 == True:
+                        val1 = True
+                if val1 == True:
+                    val2 = False
+                    for d in vga_arr:
+                        res2 = check_compatibility("vga", d, b)
+                        if res2 == True:
+                            val2 = True
+                    if val2 == True:
+                        for e in hard_disk_arr:
+                            build_arr.append("new")
+                            build_arr.append("cpu")
+                            build_arr.append(a)
+                            build_arr.append("motherboard")
+                            build_arr.append(b)
+                            build_arr.append("ram")
+                            build_arr.append(c)
+                            build_arr.append("vga")
+                            build_arr.append(d)
+                            build_arr.append("hard_disk")
+                            build_arr.append(e)
+                            build_arr.append("end")
+                            break
+
+def checkPrice(word):
+    for x in word:
+        if not x.isdigit():
+            if x != ".":
+                return False
+    return True
 
 cpu = db.CPU
-for record in cpu.find().sort("points"):
-    if cpu_min <= float(record['price']) and cpu_max >= float(record["price"]):
-        cpu_arr.append(record)
-build_sequence(cpu_arr, "cpu")
+for record in cpu.find().sort("ratings"):
+    if record["price"] != "" and record["price"] != " " and not record["price"].startswith("c"):
+        # x = checkPrice(record["price"])
+        # if x == True:
+            if float(cpu_min) <= float(record['price']) and float(cpu_max) >= float(record["price"]):
+                cpu_arr.append(record)
 
 motherboard = db.Motherboard
-for record in motherboard.find().sort("points"):
-    if motherboard_min <= float(record['price']) and motherboard_max >= float(record["price"]):
-        motherboard_arr.append(record)
-build_sequence(motherboard_arr, "motherboard")
+for record in motherboard.find().sort("ratings"):
+    if record["price"] != "" and record["price"] != " " and not record["price"].startswith("c"):
+        # x = checkPrice(record["price"])
+        # if x == True:
+            if float(motherboard_min) <= float(record['price']) and float(motherboard_max) >= float(record["price"]):
+                motherboard_arr.append(record)
 
 ram = db.RAM
-for record in ram.find().sort("points"):
-    if ram_min <= float(record['price']) and ram_max >= float(record["price"]):
-        ram_arr.append(record)
-build_sequence(ram_arr, "ram")
+for record in ram.find().sort("ratings"):
+    if record["price"] != "" and record["price"] != " " and not record["price"].startswith("c"):
+        # x = checkPrice(record["price"])
+        # if x == True:
+            if float(ram_min) <= float(record['price']) and float(ram_max) >= float(record["price"]):
+                ram_arr.append(record)
 
 vga = db.VGA
-for record in vga.find().sort("points"):
-    if vga_min <= float(record['price']) and vga_max >= float(record["price"]):
-        vga_arr.append(record)
-build_sequence(vga_arr, "vga")
+for record in vga.find().sort("ratings"):
+    if record["price"] != "" and record["price"] != " " and not record["price"].startswith("c"):
+        # x = checkPrice(record["price"])
+        # if x == True:
+            if float(vga_min) <= float(record['price']) and float(vga_max) >= float(record["price"]):
+                vga_arr.append(record)
 
 hard_disk = db.Hard_Disk
-for record in hard_disk.find().sort("points").limit(1):
-    if hard_disk_min <= float(record['price']) and hard_disk_max >= float(record['price']):
-        hard_disk_arr.append(record)
-        build_arr.append("hard_disk")
-        build_arr.append(record)
+for record in hard_disk.find().sort("ratings"):
+    if record["price"] != "" and record["price"] != " " and not record["price"].startswith("c"):
+        # x = checkPrice(record["price"])
+        # if x == True:
+            if float(hard_disk_min) <= float(record['price']) and float(hard_disk_max) >= float(record["price"]):
+                hard_disk_arr.append(record)
 
-# for x in build_arr:
-#     print(x)
+build_sequence()
+
+for p in build_arr:
+    print(p)
